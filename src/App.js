3b733642104dbe7ff5130a12f7a767797f3164d9
Vehikl Team <nomorobo-vehikl@gmail.com>
2021-03-24 17:19:48 -0400

    add month pagination

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import Day from './Day'
 import dayjs from 'dayjs'
+import { useState } from 'react'
 
 function App() {
-  const days = Array.from({length: dayjs().daysInMonth()}, (_, index) => ++index)
-  const daysOfTheWeeks = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const [monthOffset, setMonthOffset] = useState(0)
+
+  const selectedMonth = dayjs().add(monthOffset, 'M')
+
+  const days = Array.from({length: selectedMonth.daysInMonth()}, (_, index) => ++index)
+  const daysOfTheWeeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  const addMonth = () => setMonthOffset(monthOffset + 1)
+  const subtractMonth = () => setMonthOffset(monthOffset - 1)
+  const goToCurrentMonth = () => setMonthOffset(0)
+  const startDay = parseInt(selectedMonth.startOf('month').format('d')) + 1
+  console.log({ startDay })
+  console.log({ startDay: dayjs().format('dddd') })
+
+  // get current day of the week (Mon, Tues, Wed, ...)
+  // figure out it's offset (0, 1, 2, ...)
+
+
+  // get the number of days before the offset
+  // use the number of days before offset to offset the grid
 
   return (
     <div className="month flex flex-col items-center justify-center h-screen">
-      <h1>Month</h1>
-      {daysOfTheWeeks.map(day => (
-          <div>{day}</div>
-      ))}
+      <h1>{selectedMonth.format('MMMM YYYY')}</h1>
+      <div className='flex flex-row'>
+        <button className='p-3' onClick={subtractMonth}>Previous month</button>
+        <button className='p-3' onClick={goToCurrentMonth}>Now</button>
+        <button className='p-3' onClick={addMonth}>Next month</button>
+      </div>
+      
       <div className="week grid grid-cols-7 gap-3 w-3/4">
-        {days.map(day => (
-          <Day key={day} day={day} />
+        {daysOfTheWeeks.map(day => (
+            <div>{day}</div>
         ))}
+
+        {days.map((day, index) => {
+            if (index === 0) return <Day className={`col-start-${startDay}`} key={day} day={day} />
+    
+            return <Day key={day} day={day} />
+        })}   
       </div>
     </div>
   );
--- src/Day.jsx
+++ src/Day.jsx
@@ -1,6 +1,8 @@
-function Day({ day }) {
+import cn from 'classnames'
+
+function Day({ className, day }) {
     return (
-        <div className="h-40 day border p-2 relative">
+        <div className={cn(className, 'h-40 day border p-2 relative')}>
             <div className="flex flex-row justify-between">
                 <div className="text-base">{day}</div>
                 <button>+</button>
