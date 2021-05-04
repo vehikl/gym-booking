4bb72e4e955c2590372ccdec113e3a80d5f7530e
Tyler Bauman <t.bauman@vehikl.com>
2021-05-04 15:35:47 -0400

    styling booking form

--- src/Components/AddBooking.jsx
+++ src/Components/AddBooking.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import TimePicker from './TimePicker'
+import { XIcon } from '@heroicons/react/solid'
 
 function AddBooking({ visible, date, onSubmit, onCancel }) {
   const [name, setName] = useState('')
@@ -18,16 +19,25 @@ function AddBooking({ visible, date, onSubmit, onCancel }) {
 
   return visible ? (
     <div className="h-screen w-screen absolute bg-gray-200 bg-opacity-40 inset-0 flex justify-center items-center">
-      <div className="bg-white rounded shadow p-5 flex flex-col">
-        <div className="flex flex-row items-center justify-between">
-          <h1 className="font-bold text-lg pb-4">Add Your Booking for {getDate()}</h1>
-          <button onClick={onCancel} className="text-sm">Cancel</button>
-        </div>
-        <form className="flex flex-col bg-gray-200 px-5 py-3 rounded">
-          <div className="flex justify-between items-center">
-            <label htmlFor="name" className="py-3">Name:</label> 
-            <input id="name" type="text" onChange={(e) => setName(e.target.value)}/>
+      <div className="bg-white rounded shadow p-5 flex flex-col w-2/3 max-w-md">
+        <div className="flex flex-row items-center justify-between mb-4">
+          <div className="flex flex-col mr-6">
+            <h1 className="font-bold text-gray-800 text-lg">Add booking</h1>
+            <p className="text-gray-700">Booking for {getDate()}</p>
           </div>
+          
+          <button onClick={onCancel} className="text-sm text-gray-600 hover:text-gray-400">
+            <XIcon className="h-6 w-6" />
+          </button>
+          
+        </div>
+        
+        <form className="flex flex-col">
+          <label className="py-3 flex flex-col">
+            <span className="text-xs uppercase tracking-wide mb-1 font-semibold text-gray-700">Name</span>
+            <input type="text" className="border border-gray-300 rounded p-2" onChange={(e) => setName(e.target.value)}/>
+          </label> 
+
           <div className="py-3">
             <TimePicker label="Start Time" value={startTime} onChange={setStartTime} />
           </div>
