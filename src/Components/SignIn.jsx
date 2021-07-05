34b80ceee141544635afbbec56045be66d2c1e36
Vehikl Team <go@vehikl.com>
2021-07-05 16:57:17 -0400

    setting up google auth

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,10 +1,34 @@
-import 'firebaseui/dist/firebaseui.css';
+import { useEffect } from 'react';
 
 function SignIn() {
+  const onMessage = (e) => {
+    if (e.origin !== window.origin) {
+      return;
+    }
+
+    console.log(e);
+    console.log('TOKEN: ', e.data.token);
+    console.log('NAME:', e.data.name);
+    // localStorage.setItem('user',e.data.name)
+    // localStorage.setItem('jwt',e.data.token)
+  };
+
+  useEffect(() => {
+    window.addEventListener('message', onMessage, false);
+    return () => {
+      window.removeEventListener('message', onMessage);
+    };
+  }, []);
+
+  const handleOnClick = () => {
+    window.open(`${process.env.REACT_APP_API_BASE_URL}/login/google/`);
+  };
+
   return (
     <div className="h-screen w-screen items-center justify-center flex">
-      <div id="firebaseui-auth-container" />
-      <div id="loader">Loading...</div>
+      <button type="button" onClick={handleOnClick} className="border rounded p-2">
+        Sign in
+      </button>
     </div>
   );
 }
